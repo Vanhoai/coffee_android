eeba625c85b205b8552dd023a93b68c2317d7ba5
tien <1204thuytien@gmail.com>
2022-11-08 23:57:14 +0700

    tien update sliderImage

--- app/src/main/java/com/example/coffee/adapters/SlideOnBoardAdapter.java
+++ app/src/main/java/com/example/coffee/adapters/SlideOnBoardAdapter.java
@@ -1,4 +1,61 @@
 package com.example.coffee.adapters;
 
-public class SlideOnBoardAdapter {
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.bumptech.glide.Glide;
+import com.example.coffee.R;
+import com.example.coffee.models.Others.SliderImage;
+
+import java.util.ArrayList;
+
+public class SlideOnBoardAdapter extends PagerAdapter {
+    private Context context;
+    private ArrayList<SliderImage> ListSlider_Image;
+
+    public SlideOnBoardAdapter(Context context, ArrayList<SliderImage> listSlider_Image) {
+        this.context = context;
+        ListSlider_Image = listSlider_Image;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater layoutInflater = ((Activity) context).getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.item_slider_image, container, false);
+        ImageView ivhinh = view.findViewById(R.id.ivislider);
+
+        SliderImage slider = ListSlider_Image.get(position);
+        if (slider != null) {
+            Glide.with(context).load(slider.getResoure_image()).into(ivhinh);
+        }
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public int getCount() {
+        if (ListSlider_Image != null) {
+            return ListSlider_Image.size();
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
 }
--- app/src/main/java/com/example/coffee/models/Others/SliderImage.java
+++ app/src/main/java/com/example/coffee/models/Others/SliderImage.java
@@ -1,4 +1,17 @@
 package com.example.coffee.models.Others;
 
 public class SliderImage {
+    private int resoure_image;
+
+    public SliderImage(int resoure_image) {
+        this.resoure_image = resoure_image;
+    }
+
+    public int getResoure_image() {
+        return resoure_image;
+    }
+
+    public void setResoure_image(int resoure_image) {
+        this.resoure_image = resoure_image;
+    }
 }
