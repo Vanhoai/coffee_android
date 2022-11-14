d9db0be9df0556bd05b61cf1fde952a33217eb37
tien <1204thuytien@gmail.com>
2022-11-14 15:49:33 +0700

    update

--- app/src/main/java/com/example/coffee/interfaces/BaseAPI.java
+++ app/src/main/java/com/example/coffee/interfaces/BaseAPI.java
@@ -1,7 +1,7 @@
 package com.example.coffee.interfaces;
 
 public interface BaseAPI {
-
-    String BASE_URL = "http://192.168.1.108:8080/api/v1";
+    
+    String BASE_URL = "https://7fdc-113-22-115-4.ap.ngrok.io/api/v1";
 
 }
