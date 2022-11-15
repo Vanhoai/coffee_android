30b9492ea8a505777627c1d1cfee8aca8196ba70
leminhlan2003 <117093712+leminhlan2003@users.noreply.github.com>
2022-11-15 21:55:31 +0700

    Promo PlaceList lan

--- app/src/main/java/com/example/coffee/screens/bottom/Shop/PlaceListActivity.java
+++ app/src/main/java/com/example/coffee/screens/bottom/Shop/PlaceListActivity.java
@@ -1,16 +1,94 @@
 package com.example.coffee.screens.bottom.Shop;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static java.security.AccessController.getContext;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.coffee.R;
+import com.example.coffee.adapters.RecycleNearlyAdapter;
+import com.example.coffee.adapters.RecycleProductAdapter;
+import com.example.coffee.models.Product.Comment;
+import com.example.coffee.models.Product.Product;
+import com.example.coffee.models.Shop.Shop;
+import com.example.coffee.models.User.User;
+import com.example.coffee.screens.bottom.MainActivity;
+import com.example.coffee.screens.bottom.Profile.HistoryActivity;
+import com.example.coffee.utils.Storage;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class PlaceListActivity extends AppCompatActivity {
+
+    ImageView backNavigation;
+    RecyclerView recyclePlaceList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_list);
+
+        recyclePlaceList = findViewById(R.id.recyclePlaceList);
+        backNavigation = findViewById(R.id.backNavigation);
+
+
+        backNavigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PlaceListActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ArrayList<Product> products = new ArrayList<>();
+        ArrayList<Comment> comments = new ArrayList<>();
+        comments.add(new Comment());
+        comments.add(new Comment());
+        comments.add(new Comment());
+        comments.add(new Comment());
+
+        products.add(new Product(1, "The Coffee Storm", "", 25000, 100, 100, comments));
+        products.add(new Product(1, "The Coffee Storm", "", 25000, 100, 100, comments));
+        products.add(new Product(1, "The Coffee Storm", "", 25000, 100, 100, comments));
+        products.add(new Product(1, "The Coffee Storm", "", 25000, 100, 100, comments));
+        products.add(new Product(1, "The Coffee Storm", "", 25000, 100, 100, comments));
+
+        ArrayList<Shop> shops = new ArrayList<>();
+        shops.add(new Shop(1, "Tân Bình Ditrict", "", "", products, 1, 1));
+        shops.add(new Shop(1, "Tân Bình Ditrict", "", "", products, 1, 1));
+        shops.add(new Shop(1, "Tân Bình Ditrict", "", "", products, 1, 1));
+        shops.add(new Shop(1, "Tân Bình Ditrict", "", "", products, 1, 1));
+        shops.add(new Shop(1, "Tân Bình Ditrict", "", "", products, 1, 1));
+
+
+        renderProduct(recyclePlaceList, products);
+
+
     }
-}
+
+    public void renderProduct(RecyclerView recyclerView, ArrayList<Product> data) {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(PlaceListActivity.this) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
+        recyclerView.setLayoutManager(linearLayoutManager);
+        RecycleProductAdapter adapter = new RecycleProductAdapter(PlaceListActivity.this,data);
+        recyclerView.setAdapter(adapter);
+    }
+    }
+
