f30b8d35dc5575add45e1a068cba4e0e3d7dc055
giahuy2109 <huydhgps23981@fpt.edu.vn>
2022-11-18 05:52:50 +0700

    Update ShopFragment.java

--- app/src/main/java/com/example/coffee/screens/bottom/Shop/ShopFragment.java
+++ app/src/main/java/com/example/coffee/screens/bottom/Shop/ShopFragment.java
@@ -24,5 +24,4 @@ public class ShopFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
-
 }
