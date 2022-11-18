fe990c8cbea8476fc9e060886e42d8eaabd817c2
Hoai Aurora <tvhoai241223@gmail.com>
2022-11-18 20:24:41 +0700

    Revert "update"
    This reverts commit b296d13dfdcf9017f7fb8289fe316cbc3ec9deac.

--- app/src/main/java/com/example/coffee/interfaces/BaseAPI.java
+++ app/src/main/java/com/example/coffee/interfaces/BaseAPI.java
@@ -2,7 +2,7 @@ package com.example.coffee.interfaces;
 
 public interface BaseAPI {
     
-    String BASE_URL = "http://10.22.214.15:8080/api/v1";
+    String BASE_URL = "http://192.168.1.108:8080/api/v1";
 
 <<<<<<< HEAD
 =======
