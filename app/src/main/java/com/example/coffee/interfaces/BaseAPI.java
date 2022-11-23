d99da2124b8407e8c36d171ae0430db6f0e9197b
leminhlan2003 <117093712+leminhlan2003@users.noreply.github.com>
2022-11-23 17:56:41 +0700

    lan

--- app/src/main/java/com/example/coffee/interfaces/BaseAPI.java
+++ app/src/main/java/com/example/coffee/interfaces/BaseAPI.java
@@ -1,5 +1,7 @@
 package com.example.coffee.interfaces;
 
+import com.example.coffee.app.Constants;
+
 public interface BaseAPI {
 
     String BASE_URL = Constants.DEBUG ? String.format("%s%s", Constants.BASE_URL_LOCAL, Constants.API_VERSION) : String.format("%s%s", Constants.BASE_URL, Constants.API_VERSION);
